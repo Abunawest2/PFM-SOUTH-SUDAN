9310c2590aba77be1a571db700a279e1e4f920b5
Abunawest2 <121734217+Abunawest2@users.noreply.github.com>
2024-03-21 14:28:20 +0300

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,33 @@
+const openMenu = document.getElementById("menu-line");
+const closeMenus = document.getElementById("mbar");
+const hiddenMenu = document.getElementById('hidden-screen')
+
+function closeMenu(){
+    if (hiddenMenu.style.display='flex') {
+        hiddenMenu.style.display='none'
+    }
+}
+
+function displayMenu(){
+    if (hiddenMenu.style.display='none') {
+        hiddenMenu.style.display='flex'
+    }
+}
+
+const banner = document.querySelector('.banner');
+const images = ['/images/banner.jpg', '/images/blacklogo.jpg', '/images/The-Unity.jpg']; // List of image URLs
+const colors = ['black', 'rgb(255, 215, 215)', 'rgb(255, 40, 40)']
+let currentIndex = 0;
+
+function changeBackgroundImage() {
+    banner.style.color = `${colors[currentIndex]}`;
+    banner.style.backgroundImage = `linear-gradient(to top, rgba(2, 2, 2, 0.5), rgba(255, 255, 255, 0.4)), url(${images[currentIndex]})`;
+    currentIndex = (currentIndex + 1) % colors.length;
+    currentIndex = (currentIndex + 1) % images.length;
+    
+}
+
+// Initial call to set the first image
+changeBackgroundImage()
+// Change the background image every 10 seconds
+setInterval(changeBackgroundImage, 10000);// 20 seconds in milliseconds
