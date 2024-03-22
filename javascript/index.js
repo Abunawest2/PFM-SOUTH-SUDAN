5382a90f38b52233ff1915a2cfa5a0b8d5861e24
Abunawest2 <sulelegge@gmail.com>
2024-03-22 13:49:08 +0300

    HHH

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,12 @@
-const openMenu = document.getElementById("menu-line");
+const openMenu = document.getElementById('menu-line');
 const closeMenus = document.getElementById("mbar");
 const hiddenMenu = document.getElementById('hidden-screen')
+const backgroundColors = ['linear-gradient(rgba(255, 0, 0, 0.5), rgba(255, 0, 0, 0.5))', 'linear-gradient(rgba(0, 255, 115, 0.5), rgba(0, 255, 115, 0.5))', 'linear-gradient(rgba(50, 0, 50, 0.5), rgba(50, 0, 50, 0.5))', 'linear-gradient(rgba(255, 230, 50, 0.6), rgba(255, 230, 50, 0.6))']
+const sectionHome = document.querySelector('.section-home');
+const banner = document.querySelector('.banner');
+const images = ['../images/banner.jpg', '../images/blacklogo.jpg', '../images/The-Unity.jpg']; // List of image URLs
+let currentIndex = 0;
+let index = 0
 
 function closeMenu(){
     if (hiddenMenu.style.display='flex') {
@@ -14,34 +20,24 @@ function displayMenu(){
     }
 }
 
-const banner = document.querySelector('.banner');
-const images = ['../images/banner.jpg', '../images/blacklogo.jpg', '../images/The-Unity.jpg']; // List of image URLs
-const colors = ['black', 'rgb(255, 215, 215)', 'blackrgb(175, 20, 0)']
-let currentIndex = 0;
 
 function changeBackgroundImage() {
-    banner.style.color = `${colors[currentIndex]}`;
+    sectionHome.style.backgroundImage = backgroundColors[index];
     banner.style.backgroundImage = `linear-gradient(to top, rgba(255, 0, 0, 0.1), rgba(255, 0, 0, 0.7)), url(${images[currentIndex]})`;
-    currentIndex = (currentIndex + 1) % colors.length;
     currentIndex = (currentIndex + 1) % images.length;
-    
+    index = (index + 1) % backgroundColors.length;
+}
+
+function changeColors() {
+    banner.style.color = `${colors[currentIndex]}`;
+    sectionHome.style.color = `${colors[index]}`;
 }
 
 // Initial call to set the first image
 changeBackgroundImage()
 // Change the background image every 10 seconds
-setInterval(changeBackgroundImage, 5000);// 20 seconds in milliseconds
-
-const backgroundColors = ['linear-gradient(rgba(255, 0, 0, 0.5), rgba(255, 0, 0, 0.5))', 'linear-gradient(rgba(0, 255, 115, 0.5), rgba(0, 255, 115, 0.5))', 'linear-gradient(rgba(50, 0, 50, 0.5), rgba(50, 0, 50, 0.5))', 'linear-gradient(rgba(255, 230, 50, 0.6), rgba(255, 230, 50, 0.6))']
-const sectionHome = document.querySelector('.section-home');
-
-
-function changeColors() {
-    sectionHome.style.backgroundImage = backgroundColors[currentIndex];
-    currentIndex = (currentIndex + 1) % backgroundColors.length;
-    sectionHome.style.color = `${colors[currentIndex]}`;
-}
+setInterval(changeBackgroundImage, 1000);// 20 seconds in milliseconds
 
 changeColors()
 // Change the background image every 10 seconds
-setInterval(changeColors, 5000);
+setInterval(changeColors, 1000);
